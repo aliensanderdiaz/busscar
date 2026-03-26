fbe35e8fa5e57e4e71e870fac8fa2042590e0689
aliensanderdiaz <aliensanderdiaz@gmail.com>
2026-03-25 20:45:24 -0500

    m

--- script2.js
+++ script2.js
@@ -1,26 +1,28 @@
-console.log({length: perfiles.length})
+console.log({ length: perfiles.length })
 let perfilesMostrar = perfiles.filter(perfil => perfil.lugar !== 'nnon').slice(0, 49)
-console.log({length: perfiles.length})
+console.log({ length: perfiles.length })
 
 const codificar = (entrada) => {
-  const entradaArray = entrada.split('')
-  return entradaArray.map(_ => _ + 'n').join('')
+    const entradaArray = entrada.split('')
+    return entradaArray.map(_ => _ + 'n').join('')
 }
 
 const decodificar = (entrada) => {
-  const entradaArray = entrada.split('')
-  return entradaArray.filter((_, index) => index % 2 === 0).join('')
+    const entradaArray = entrada.split('')
+    return entradaArray.filter((_, index) => index % 2 === 0).join('')
 }
 
 const contenedor = document.querySelector('.contenedor')
 
-let html = ''
+const reset = () => {
 
-for (const element of perfilesMostrar) {
-    let telefonoMostrar = element.otrosNumeros[element.otrosNumeros.length - 1] || element.celular
-    html += `
+    let html = ''
+    let index = 1
+    for (const element of perfilesMostrar) {
+        let telefonoMostrar = element.otrosNumeros[element.otrosNumeros.length - 1] || element.celular
+        html += `
         <div class="card">
-            <p>
+            <p> ${index} - 
                 <a 
                 href="https://www.google.com/search?q=${decodificar(telefonoMostrar)}" 
                 target="_blank" 
@@ -40,30 +42,35 @@ for (const element of perfilesMostrar) {
                 
             </p>
         </div>`
+        index++
+    }
+
+    // ${element.otrosNumeros.map(numero => decodificar(numero)).join('<br>')} <br>
+    // ${element.codigos.map(codigo => decodificar(codigo)).join('<br>')} <br>
+
+    contenedor.innerHTML = html
 }
 
-                // ${element.otrosNumeros.map(numero => decodificar(numero)).join('<br>')} <br>
-                // ${element.codigos.map(codigo => decodificar(codigo)).join('<br>')} <br>
+reset()
 
-contenedor.innerHTML = html
 
 const inputSearch = document.querySelector('#inputSearch')
 
 
 const buscar = () => {
     const valueSearch = codificar(inputSearch.value)
-    
+
     const perfil = perfiles.find(perfil => {
         return perfil.celular === valueSearch || perfil.otrosNumeros.includes(valueSearch) || perfil.codigos.includes(valueSearch)
-    
+
     })
     console.log({ perfil })
     if (!perfil) {
-        contenedor.innerHTML = `<div><p>${inputSearch.value} No encontrado</p></div>`
+        contenedor.innerHTML = `<div class="card"><p>${inputSearch.value} No encontrado</p></div><div class="card"><button class="btn-full" onclick="reset()">Reset</button></div>`
     } else {
         let telefonoMostrar = perfil.otrosNumeros[perfil.otrosNumeros - 1] || perfil.celular
         contenedor.innerHTML = `
-        <div>
+        <div class="card">
             <p>
                 <a 
                 href="https://www.google.com/search?q=${decodificar(telefonoMostrar)}" 
@@ -83,6 +90,6 @@ const buscar = () => {
 
                 
             </p>
-        </div>`
+        </div><div class="card"><button class="btn-full" onclick="reset()">Reset</button></div>`
     }
 }
