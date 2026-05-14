0619a75831279271b3246b4e298c8958cadc2551
aliensanderdiaz <aliensanderdiaz@gmail.com>
2026-05-14 06:29:26 -0500

    m

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
 console.log({ length: perfiles.length })
-let perfilesMostrar = perfiles.filter(perfil => perfil.lugar !== 'nnon').slice(0, 160)
+let perfilesMostrar = perfiles.filter(perfil => perfil.lugar !== 'nnon').slice(0, 163)
 console.log({ length: perfiles.length })
 
 const codificar = (entrada) => {
