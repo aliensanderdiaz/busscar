64f1ca42c8381a7f324593def2e50b7674fe3b92
aliensanderdiaz <aliensanderdiaz@gmail.com>
2026-07-28 12:58:03 -0500

    m

--- script5.js
+++ script5.js
@@ -2,17 +2,19 @@
 
 const app = document.getElementById("app");
 
-app.innerHTML = perfiles.map(perfil => `
+const total = perfiles.length
+
+app.innerHTML = perfiles.map((perfil, index) => `
     <section class="perfil">
 
         <div class="perfil-info">
-            <h2>${perfil.lugar}</h2>
+            <h2>${ index + 1 } de ${ total } - ${perfil.lugar}</h2>
             <p>${perfil.detalles}</p>
         </div>
 
         <div class="galeria">
             ${perfil.codes.map(item => `
-                <div class="card">
+                <div class="card ${ perfil.lugar === 'no' ? 'advertencia':'normal'}">
                 <div class="indice">${item.indice}</div>
                     <a href="https://co.mileroticos.com/escorts/x/${ item.code }" target="_blank">
                     <img src="https://static1.mileroticos.com/photos/l1/${ item.image}"
